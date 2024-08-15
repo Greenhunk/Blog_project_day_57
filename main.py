2d5ae24618c86faf7c2c2404a327c2d1de2ff8e8
Greenhunk <tanvir.hasan77@yahoo.com>
2024-08-16 00:51:25 +0300

    fixed issues on main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    data = response.json()
+    return render_template("index.html", blog_data = data)

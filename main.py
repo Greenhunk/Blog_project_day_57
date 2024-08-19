d461813770308e13711cab05a32f7b22ed479af1
Md Tanvir Hasan <Md.Tanvir.Hasan@lut.fi>
2024-08-19 09:07:18 +0300

    updated main.py

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def home():
     response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
     data = response.json()
     return render_template("index.html", blog_data = data)
+
+@app.route('/post/<int:post_id>')
+def read(post_id):
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    data = response.json()
+    return render_template("post.html", blog_data_1 = data, blog_id = post_id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
